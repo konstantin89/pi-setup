af541ef410e23398d25303d7a99455cd01d47df8
Kosta Bulgakov <kosta.bulgakov@mail.huji.ac.il>
2023-09-24 11:04:05 -0900

    wip

---
+++ templates/kernel_module/hello_kernel_module.c
@@ -0,0 +1,20 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Your Name");
+MODULE_DESCRIPTION("Hello Kernel Module");
+
+static int __init hello_init(void) 
+{
+    printk(KERN_INFO "Hello, Kernel!\n");
+    return 0; // Non-zero return means module initialization failed
+}
+
+static void __exit hello_exit(void) 
+{
+    printk(KERN_INFO "Goodbye, Kernel!\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
