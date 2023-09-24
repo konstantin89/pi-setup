b3f7166d9c621ef98372f1133c2a977d66d37f85
Kosta Bulgakov <kosta.bulgakov@mail.huji.ac.il>
2023-09-24 04:26:43 -0900

    wip

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 # Make sure that install dir is part of $PATH
-INSTALL_PATH='/home/pi/.local/bin'
+INSTALL_PATH='/usr/local/games'
 
 ## Install the script utils
-rm "$INSTALL_PATH"/kosta_*
+sudo rm "$INSTALL_PATH"/kosta_*
 
-cp "./scripts/kosta_makefile.sh" "$INSTALL_PATH"
-cp "./scripts/kosta_help.sh" "$INSTALL_PATH"
-cp "./scripts/kosta_git_push.sh" "$INSTALL_PATH"
+sudo cp "./scripts/kosta_makefile.sh" "$INSTALL_PATH"
+sudo cp "./scripts/kosta_help.sh" "$INSTALL_PATH"
+sudo cp "./scripts/kosta_git_push.sh" "$INSTALL_PATH"
 
 
 ## Edit bashrc file
 source ./edit_bashrc.sh
+
+## install packages
+sudo apt -y install raspberrypi-kernel-headers
+sudo apt -y install cmake
